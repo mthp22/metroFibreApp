e67880cbb3a9a6906ed341698b8d03d319b98485
mthp22 <221026639@students.uj.ac.za>
2026-05-12 12:55:24 +0200

    backend: add main script and updated environment settings

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,46 @@
+using backend.Application.Interfaces;
+using backend.Application.Services;
+using backend.Infrastructure.Database;
+using backend.Infrastructure.Initialization;
+using backend.Infrastructure.Repositories;
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<MongoDbSettings>(builder.Configuration.GetSection("MongoDbSettings"));
+var mongoSettings = builder.Configuration.GetSection("MongoDbSettings").Get<MongoDbSettings>() ?? new MongoDbSettings();
+var mongoClient = new MongoClient(mongoSettings.ConnectionString);
+var mongoDatabase = mongoClient.GetDatabase(mongoSettings.DatabaseName);
+
+builder.Services.AddSingleton<IMongoDatabase>(mongoDatabase);
+builder.Services.AddScoped<IIngredientRepository, IngredientRepository>();
+builder.Services.AddScoped<IRecipeRepository, RecipeRepository>();
+builder.Services.AddScoped<IMealPlanResultRepository, MealPlanResultRepository>();
+builder.Services.AddScoped<IMealPlannerService, MealPlannerService>();
+builder.Services.AddScoped<MongoDbInitializer>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("frontend", policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+using (var scope = app.Services.CreateScope())
+{
+    var initializer = scope.ServiceProvider.GetRequiredService<MongoDbInitializer>();
+    await initializer.InitializeAsync();
+}
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("frontend");
+app.MapControllers();
 app.Run();
