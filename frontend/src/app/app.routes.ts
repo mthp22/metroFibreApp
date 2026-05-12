c7ccf14eaac76c7548fafabf3dfc301094805ec7
mthp22 <221026639@students.uj.ac.za>
2026-05-12 13:54:08 +0200

    frontend: add updated app structure and routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MealPlanner } from './pages/meal-planner/meal-planner';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: '', 
+        component: MealPlanner 
+    }
+];
